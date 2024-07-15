589b12f5011ff3314a98d542c43f276d741f3c2d
Ilham Maulana Putra <52365731+ilhamaulanap@users.noreply.github.com>
2024-07-15 17:38:00 +0700

    add volumes to kafka container

--- scripts/kafka_setup.sh
+++ scripts/kafka_setup.sh
@@ -1,4 +1,17 @@
-echo "Running Kafka using docker"
-cd streamcommerce/kafka
-docker-compose up -d
+#!/bin/bash
 
+# Fetch external IP address using metadata server
+EXTERNAL_IP=$(curl -s -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+
+# Export as KAFKA_ADDRESS
+export KAFKA_ADDRESS=$EXTERNAL_IP
+
+# Verify the exported IP address
+echo "Exported KAFKA_ADDRESS: $KAFKA_ADDRESS"
+
+# Change directory to where your docker-compose.yml is located
+echo "Running Kafka using docker-compose"
+cd streamcommerce/kafka
+
+# Start Kafka and Zookeeper containers
+docker-compose up -d
