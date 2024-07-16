3af4bd9ecae550b33475d25fa559d2c6e1577eb6
Ilham Maulana Putra <52365731+ilhamaulanap@users.noreply.github.com>
2024-07-16 21:22:07 +0700

    change docker-compose version to 3

--- scripts/vm_setup.sh
+++ scripts/vm_setup.sh
@@ -2,7 +2,7 @@ echo "Running sudo apt-get update..."
 sudo apt-get update
 
 echo "Installing Docker..."
-sudo apt-get -y install docker.io python3-pip
+sudo apt-get -y install docker.io python3-pip docker-compose
 
 echo "Docker without sudo setup..."
 sudo groupadd docker
