308ab434217b7df5de86d26bc920d5dcb184bbf3
Ilham Maulana Putra <52365731+ilhamaulanap@users.noreply.github.com>
2024-07-16 22:41:37 +0700

    fix dockerfile

--- scripts/airflow_setup.sh
+++ scripts/airflow_setup.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
+echo "Running sudo apt-get update..."
+sudo apt-get update
+
+echo "Installing Docker..."
+sudo wget -qO- https://get.docker.com/ | sh
+
+echo "Docker without sudo setup..."
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+sudo service docker restart
+
+echo "Cloning git repository"
+git clone https://github.com/ilhamaulanap/streamcommerce.git
+
+
 # Change directory to where your docker-compose.yml is located
 echo "Running Kafka using docker-compose"
 cd streamcommerce/airflow
-docker-compose build
+docker compose build
 
 echo "Running airflow-init"
-docker-compose up airflow-init
+docker compose up airflow-init
 
 # Start airflow containers
 echo "Starting Kafka using docker-compose"
